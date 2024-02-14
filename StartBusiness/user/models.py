ec0e539bc803950c10219924b7573879ac0b6f11
Aman Yadav <virajgurjar789@gmail.com>
2024-02-14 21:15:40 +0530

    user response changes

--- StartBusiness/user/models.py
+++ StartBusiness/user/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.hashers import make_password
 
 class User(models.Model):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_email = models.EmailField(max_length=225, unique=True)
-    user_mobile_number = models.CharField(max_length=225,unique=True)
+    user_email = models.EmailField(max_length=225)
+    user_mobile_number = models.CharField(max_length=225)
     user_password = models.CharField(max_length=225)
     is_verify = models.BooleanField(default=False)
     user_role = models.CharField(max_length=225)
