34574794eaa9cbecf7c00c3e7da0bef9ff6ec488
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-02-15 15:47:50 +0530

    user name field added

--- StartBusiness/user/models.py
+++ StartBusiness/user/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_email = models.EmailField(max_length=225)
     user_mobile_number = models.CharField(max_length=225)
-    user_name = models.CharField(max_length=225)
+    user_name = models.CharField(max_length=225 , default=None)
     user_password = models.CharField(max_length=225)
     is_verify = models.BooleanField(default=False)
     user_role = models.CharField(max_length=225)
