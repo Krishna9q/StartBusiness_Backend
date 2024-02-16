eb2064ca838d531c8655d231bc7ec366217c520a
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-02-16 22:17:20 +0530

    forget password

--- StartBusiness/user/urls.py
+++ StartBusiness/user/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('update/<uuid:input>/', UserUpdateView.as_view(), name = 'user update'),
     path('login/', UserLoginView.as_view(), name = 'user login'),
     path('otp-verification/<uuid:input>/', UserOtpVerificationEmail.as_view(), name = 'user view by id'),
+    path('forgetPassword/<uuid:input>/', ForgetPassword.as_view(), name = 'forget Password view'),
     path('forgetPassword/', ForgetPassword.as_view(), name = 'forget Password view'),
     path('otp-resend/', UserOtpResend.as_view(), name = 'otp resend by email  id'),
     path('otp-resend/<uuid:input>/', UserOtpResend.as_view(), name = 'otp resend by email  id'),
