db82a46c19e2c8450cd7b81cb2b7e5853c4acf02
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-02-21 11:23:27 +0530

    changes in model

--- StartBusiness/category/models.py
+++ StartBusiness/category/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 import uuid
-
+from django.utils.translation import gettext_lazy as _
 class Category(models.Model):
     category_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     category_name = models.CharField(max_length=225)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    category_image = models.ImageField(upload_to='category/',default=None) 
+    category_image = models.ImageField(_('image'),upload_to='category/',default=None) 
     
