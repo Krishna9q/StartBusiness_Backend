dfae659b2f276fc646ca7201cb6a26d616794869
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-02-28 11:40:01 +0530

    dealer changes

--- StartBusiness/dealer/models.py
+++ StartBusiness/dealer/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Dealer(models.Model):
     dealer_id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     dealer_name = models.CharField(max_length=225)
-    dealer_email = models.EmailField(max_length=225, unique=True)
-    dealer_mobile_number = models.CharField(max_length=10,unique=True)
+    dealer_email = models.EmailField(max_length=225, )
+    dealer_mobile_number = models.CharField(max_length=10,)
     dealer_address = models.TextField(blank=True)
     dealer_image = models.TextField(blank=True)
     dealer_description = models.TextField(blank=True)
--- StartBusiness/dealer/views.py
+++ StartBusiness/dealer/views.py
@@ -4,11 +4,19 @@ from dealer.serializers import DealerSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from StartBusiness.s3_image_config import upload_base64_file
+from rest_framework import status
 
 # add dealer
 class DealerAddView(GenericAPIView):
     serializer_class = DealerSerializer
     def post(self , request):
+        if Dealer.objects.filter(dealer_email=request.data.get('dealer_email')).count() >=1 or Dealer.objects.filter(dealer_mobile_number=request.data.get('dealer_mobile_number')).count() >=1:
+          
+              return Response({
+                                  'status' : status.HTTP_400_BAD_REQUEST,
+                                   'message' : "this user is already registerd"
+                      },status=400)
+        else:
             serializer = DealerSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             file = request.data.get('dealer_image')
