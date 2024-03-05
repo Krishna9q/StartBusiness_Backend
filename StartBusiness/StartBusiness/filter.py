31a48b1af3818947599446d59345e4dae7882472
Aman Yadav <113305877+AmanYadav96@users.noreply.github.com>
2024-03-05 22:46:36 +0530

    filters

--- StartBusiness/StartBusiness/filter.py
+++ StartBusiness/StartBusiness/filter.py
@@ -9,6 +9,7 @@ import django_filters
 class BrandFilter(FilterSet):
     brand_name = django_filters.CharFilter(field_name='brand_name', lookup_expr='icontains')
     is_active = django_filters.BooleanFilter(field_name='is_active')
+    # category = django_filters.UUIDFilter(field_name='category' )
 
     class Meta:
         model = Brand
