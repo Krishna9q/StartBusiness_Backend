4b215ba4afe18c4cd1ab47773c9e0311a0256c65
Kuldeep Yadav <kuldeepy.bca2022@ssism.org>
2024-04-22 15:52:31 +0530

    Added cart app with models, views, and URL configurations

--- StartBusiness/cart/models.py
+++ StartBusiness/cart/models.py
@@ -1,3 +1,12 @@
+import uuid
 from django.db import models
+from product.models import Product
+from user.models import User
 
-# Create your models here.
+class Cart(models.Model):
+    cart_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False,unique=True)
+    quantity = models.PositiveIntegerField()
+    price = models.PositiveBigIntegerField()
+    total_amount = models.PositiveBigIntegerField()
+    product = models.ForeignKey(Product,default=uuid.uuid4,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,default=uuid.uuid4,on_delete=models.CASCADE)
